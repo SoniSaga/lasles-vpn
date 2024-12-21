546084170785a5ce43ca68070abd8eb0d3309eea
Soni Bro <shantanusoni979@gmail.com>
2024-12-21 17:57:31 +0530

    Adding header

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -5,4 +5,28 @@ const config = {
   },
 };
 
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: [
+      "./pages/*.js",
+      "./pages/**/*.js",
+      "./components/*.js",
+      "./components/**/*.js",
+    ],
+    whitelistPatterns: [/^slick-/],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
+
+module.exports = {
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
+
 export default config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
+import React from "react";
+import Header from "../components/Layout/Header";
 
 export default function Home() {
   return (
     <>
-       <>Hello World</>
+        <Header />
     </>
   );
 }
